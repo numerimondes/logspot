d135f2a72c4607b9ed54890bebcffd4b82aeed9c
numerimondes <info@numerimondes.com>
2024-10-13 21:44:35 +0100

    second Commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 export default ({
-    modules: ['@nuxtjs/tailwindcss', '@nuxt/content', 'nuxt-icon'],
-    content: {
-        highlight: {
-            theme: 'nord',
-            preload: ['js', 'css', 'html', 'md', 'ts', 'tsx', 'vue', 'python', 'ruby', 'java'],
-        }
-    }
-})
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', 'nuxt-icon','@nuxt/image'],
+
+  content: {
+      highlight: {
+          theme: 'nord',
+          preload: ['js', 'css', 'html', 'md', 'ts', 'tsx', 'vue', 'python', 'ruby', 'java'],
+      }
+  },
+
+  compatibilityDate: '2024-10-13'
+});
